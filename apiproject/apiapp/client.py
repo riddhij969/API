c18214ecdc7ee657f3551d9bc8a492f9059dac59
Riddhi Jain <69605346+riddhij969@users.noreply.github.com>
2022-01-29 01:11:04 +0530

    Add files via upload

---
+++ apiproject/apiapp/client.py
@@ -0,0 +1,21 @@
+# import requests
+# import sys, json
+# import os
+#
+# def get_token():
+#
+#     #Get auth token
+#     url = "http://127.0.0.1:8000/api/auth/"
+#     response = requests.post(url, data= {'Username': 'Riddhi', 'Password': 'Rj@123456'})
+#     return response.json()
+#
+# def get_data():
+#
+#     url = "http://127.0.0.1:8000/api/users/"
+#     headers = {'Authorization': f'Token{get_token()}'}
+#     response = requests.get(url, headers = headers)
+#     prod_data = response.json()
+#     for i in prod_data:
+#         print(i)
+#
+# get_data()
